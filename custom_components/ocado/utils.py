9b88db1254e76aac87896f7f12933bbdff3a2d92
Ben Brooks <ben@bbrks.me>
2025-06-07 21:04:44 +0100

    Support order ref capture in "Your receipt and updates for today’s delivery" emails

--- custom_components/ocado/utils.py
+++ custom_components/ocado/utils.py
@@ -117,7 +117,7 @@ def get_edit_datetime(message: str) -> datetime:
 
 def get_order_number(message: str) -> str:
     """Parse the order number."""
-    raw = re.search(r"(?:Order\s(?:ref(?:\.|erence):|is)?(?:\s+))(?P<order_number>\d+)",message)
+    raw = re.search(r"(?:Order\s(?:ref(?:\.|erence):|no:|is)?(?:\s+))(?P<order_number>\d+)",message)
     if raw:
         return raw.group('order_number')
     _LOGGER.error("No order number retrieved from message %s.", message[:20])
